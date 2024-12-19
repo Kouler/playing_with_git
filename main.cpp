7989e1a56bd50d43e0b778ca7f0e6e554b6b6d57
Kouler <beeproger@yandex.ru>
2024-12-19 18:54:20 +1000

    new version 1.0.1.2-beta: write into console all arguments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
-int main(int argc, char const *argv[])
-{
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    for (size_t i = 0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
     
     return 0;
 }

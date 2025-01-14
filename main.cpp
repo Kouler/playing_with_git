4d1fe3fabe91824be24219832d17d923ebe2e65e
FDR <simakinfdr@yandex.ru>
2025-01-14 21:49:30 +1000

    new vetochka

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
+int foo_to_be_checked() {
+    std::cout << "Please, confirm my pull-request\n";
+}
+
 int main(int argc, char const *argv[])
 {
     std::cout << "I don't give a damn, man, that's just a constant string :)" << std::endl;
     for (size_t i = 0; i < argc; i++) {
         std::cout << argv[i] << std::endl;
     }
-    //lollolololol
+    //
+    
+
     return 0; 
 }
